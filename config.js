7aae4956aa2c38c0043028e0429a7bc8f8bcb5f7
DarshanDzns <140175381+DarshanDzns@users.noreply.github.com>
2023-08-04 21:50:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,27 +22,27 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
-global.prefa = ['-'] // single prefix
+global.botname = "𝘿 𝙀 𝙓 𝘽𝙊𝙏" 
+global.author = "𝙳 𝙸 𝚅 𝙰 𝙺 𝙰 𝚁🥷🏻"  
+global.packname = "𝘿 𝙀 𝙓 ✯ 𝘽𝙊𝙏"  
+global.myweb = "https://bento.me/darshandzns"
+global.footer = "©\t" + "DarshanDzns"
+global.prefa = ['.'] // single prefix
 global.themeemoji = "🎐"
 global.autoBio = "$mode : $time ⌚ || $date 🗓️ || $botname" // Leave it as it is if you didn't understood!
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
-global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
+global.socialLink = `*⪧⪢ https://chat.whatsapp.com/EHHWKxWzGqPG1KgBEaevBm💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
+global.ownername = "𝙳 𝙸 𝚅 𝙰 𝙺 𝙰 𝚁🥷🏻"    
+global.ownernumber = ["918767327657"]
+global.ytname = "YT: DarshanDzns"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
-global.state = "Enter Your State Name Here" // your state name
+global.region = "INDIA"
+global.state = "Maharastra" // your state name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "Enter Your Insta ID Here"
-global.email = "Enter Your Email ID Here"
+global.instagramId = "@divakar._fr"
+global.email = "divakarjainkar063@gmail.com"
 
 
 // Greeting messages
